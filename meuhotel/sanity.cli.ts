bb41ce8d474d82f389c090427cdc04f77b387bfa
Karen Oliveira <doliveira.ka@gmail.com>
2026-05-08 17:21:14 -0300

    fix: remove deprecated deployment config from sanity cli

--- meuhotel/sanity.cli.ts
+++ meuhotel/sanity.cli.ts
@@ -1,15 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'rlbqpbtu',
     dataset: 'production'
-  },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
   }
 })
