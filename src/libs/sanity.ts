59a67dbb46c5c4d6928d830ad3a7e904855bf41a
Karen Oliveira <doliveira.ka@gmail.com>
2026-05-08 18:55:35 -0300

    fix: remove token from public sanity client

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -3,8 +3,6 @@ import { createClient } from 'next-sanity';
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  token: process.env.SANITY_API_TOKEN,
-  // MUDANÇA CRUCIAL:
-  useCdn: false, // Força a API a não usar o CDN, permitindo a autenticação do token
   apiVersion: '2025-01-01',
+  useCdn: true,
 });
